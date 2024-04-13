e60c6979ae93c4ca8c3c26745be46a41607e0d81
YashKhatpe <yashkhatpe0611@gmail.com>
2024-04-13 20:21:34 +0530

    signup styling

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, Platform, KeyboardAvoidingView, Alert } from 'react-native';
-import { Input, Text } from 'react-native-elements';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Platform, Alert, Keyboard } from 'react-native';
+import { Text, Image } from 'react-native-elements';
 import { useFirebase } from '../context/AuthContext';
-import { LinearGradient } from 'expo-linear-gradient';
-
-
+import { StatusBar } from "expo-status-bar";
+import { TextInput } from "react-native-gesture-handler";
+import Animated, {  FadeInUp } from 'react-native-reanimated';
 const Signup = ({ navigation }) => {
 
   const firebase = useFirebase();
@@ -15,6 +15,8 @@ const Signup = ({ navigation }) => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [emailError, setEmailError] = useState(null);
   const [buttonColor, setButtonColor] = useState("#246BFD");
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
 
 
   const handleSignup = async () => {
@@ -51,94 +53,206 @@ const Signup = ({ navigation }) => {
     setButtonColor("#246BFD");
   }
 
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', (event) => {
+      setKeyboardHeight(event.endCoordinates.height - 380); 
+    });
+    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+      setKeyboardHeight(0);
+    });
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
 
   return (
-    <KeyboardAvoidingView>
+   // <KeyboardAvoidingView>
 
-    <LinearGradient colors={['#000070', '#000047', '#000033', '#00001f']} start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 1 }} style={styles.container}
-    >
-      <View style={styles.signupFont}>
-          <Text style={{color:'white',fontSize:45}}>
-            Sign up
-          </Text>
-        </View>
-      <View style={styles.innerContainer}>
+    // <LinearGradient colors={['#000070', '#000047', '#000033', '#00001f']} start={{ x: 0, y: 0 }}
+    //   end={{ x: 1, y: 1 }} style={styles.container}
+    // >
+    //   <View style={styles.signupFont}>
+    //       <Text style={{color:'white',fontSize:45}}>
+    //         Sign up
+    //       </Text>
+    //     </View>
+    //   <View style={styles.innerContainer}>
         
 
-        <View style={styles.inputsContainer}>
-          <Input
-            placeholder="Username"
-            leftIcon={{ type: 'font-awesome', name: 'user', color: 'white' }}
-            value={username}
-            onChangeText={setUsername}
-            autoCapitalize="none"
-            style={styles.inputs}
-            />
+    //     <View style={styles.inputsContainer}>
+    //       <Input
+    //         placeholder="Username"
+    //         leftIcon={{ type: 'font-awesome', name: 'user', color: 'white' }}
+    //         value={username}
+    //         onChangeText={setUsername}
+    //         autoCapitalize="none"
+    //         style={styles.inputs}
+    //         />
 
 
-          <Input
-            placeholder="Email"
-            leftIcon={{ type: 'font-awesome', name: 'envelope', color: 'white' }}
-            value={email}
-            onChangeText={setEmail}
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-            error={!!emailError}
-            errorStyle={{ color: 'red' }}
-            style={styles.inputs}
-            />
+    //       <Input
+    //         placeholder="Email"
+    //         leftIcon={{ type: 'font-awesome', name: 'envelope', color: 'white' }}
+    //         value={email}
+    //         onChangeText={setEmail}
+    //         keyboardType="email-address"
+    //         autoCapitalize="none"
+    //         autoCorrect={false}
+    //         error={!!emailError}
+    //         errorStyle={{ color: 'red' }}
+    //         style={styles.inputs}
+    //         />
 
 
-          <Input
-            placeholder="Phone Number"
-            leftIcon={{ type: 'font-awesome', name: 'phone', color: 'white' }}
-            value={phoneNumber}
-            onChangeText={setPhoneNumber}
-            keyboardType='numeric'
-            style={styles.inputs}
-            />
-          <Input
-            placeholder="Password"
-            leftIcon={{ type: 'font-awesome', name: 'lock', color: 'white' }}
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-            style={styles.inputs}
-            />
-          <Input
-            placeholder="Confirm Password"
-            leftIcon={{ type: 'font-awesome', name: 'lock', color: 'white' }}
-            value={confirmPassword}
-            onChangeText={setConfirmPassword}
-            secureTextEntry
-            style={styles.inputs}
-            />
-        </View>
-        <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 20, marginBottom: 10 }}>
-          <Text style={{ color: 'white' }}>Already have an account. </Text>
-          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-            <Text style={styles.login}>
-              Log In?
-            </Text>
-          </TouchableOpacity>
-        </View>
+    //       <Input
+    //         placeholder="Phone Number"
+    //         leftIcon={{ type: 'font-awesome', name: 'phone', color: 'white' }}
+    //         value={phoneNumber}
+    //         onChangeText={setPhoneNumber}
+    //         keyboardType='numeric'
+    //         style={styles.inputs}
+    //         />
+    //       <Input
+    //         placeholder="Password"
+    //         leftIcon={{ type: 'font-awesome', name: 'lock', color: 'white' }}
+    //         value={password}
+    //         onChangeText={setPassword}
+    //         secureTextEntry
+    //         style={styles.inputs}
+    //         />
+    //       <Input
+    //         placeholder="Confirm Password"
+    //         leftIcon={{ type: 'font-awesome', name: 'lock', color: 'white' }}
+    //         value={confirmPassword}
+    //         onChangeText={setConfirmPassword}
+    //         secureTextEntry
+    //         style={styles.inputs}
+    //         />
+    //     </View>
+    //     <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 20, marginBottom: 10 }}>
+    //       <Text style={{ color: 'white' }}>Already have an account. </Text>
+    //       <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+    //         <Text style={styles.login}>
+    //           Log In?
+    //         </Text>
+    //       </TouchableOpacity>
+    //     </View>
+    //     <TouchableOpacity
+    //       title="Submit"
+    //       onPressIn={handlePressIn}
+    //       onPressOut={handlePressOut}
+    //       onPress={handleSignup}
+    //       style={[styles.buttonContainer, { backgroundColor: buttonColor }]}
+    //     >
+    //       <Text style={styles.button}>
+    //         Submit
+    //       </Text>
+    //     </TouchableOpacity>
+
+    //   </View>
+    // </LinearGradient>
+  //</KeyboardAvoidingView>
+
+
+
+  <View className="bg-white h-full w-full">
+  <StatusBar style="light" />
+  <View className="w-full h-full absolute">
+    <Image className="w-full" source={require('../assets/background.png')} style={{transform: [{ translateY: keyboardHeight }],height:'95%' }} />
+  </View>
+  <View className="flex-row justify-around w-full absolute" >
+    <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify()} className="h-[220] w-[90]" source={require('../assets/light.png')} style={{transform: [{ translateY: keyboardHeight }] }} />
+    <Animated.Image entering={FadeInUp.delay(400).duration(1000).springify()} className="h-[160] w-[65]" source={require('../assets/light.png')} style={{ transform: [{ translateY: keyboardHeight }]}}/>
+  </View>
+
+  <View className="h-full w-full flex justify-around pt-48">
+
+    <View className="flex items-center">
+      <Animated.Text entering={FadeInUp.delay(200).duration(2000).springify()} className="text-white  font-bold tracking-wider text-5xl overflow-hidden">
+        SignUp
+      </Animated.Text>
+    </View>
+
+        {/* <KeyboardAvoidingView> */}
+    <View className="flex items-center mx-4 space-y-2">
+
+      <Animated.View entering={FadeInUp.delay(200).duration(2000).springify()} className="bg-black/5 p-4 rounded-2xl w-full">
+        <TextInput placeholder="Username"
+          placeholderTextColor={'gray'}
+          value={username}
+          onChangeText={setUsername}
+          autoCapitalize="none"
+          />
+      </Animated.View>
+
+      <Animated.View entering={FadeInUp.delay(400).duration(2000).springify()} className="bg-black/5 p-4 rounded-2xl w-full">
+        <TextInput placeholder="Email"
+          placeholderTextColor={'gray'}
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          error={!!emailError}
+          errorStyle={{ color: 'red' }} />
+      </Animated.View>
+
+      <Animated.View entering={FadeInUp.delay(400).duration(2000).springify()} className="bg-black/5 p-4 rounded-2xl w-full">
+        <TextInput placeholder="Phone Number"
+          placeholderTextColor={'gray'}
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+          keyboardType="numeric"/>
+      </Animated.View>
+
+
+      <Animated.View entering={FadeInUp.delay(400).duration(2000).springify()} className="bg-black/5 p-4 rounded-2xl w-full">
+        <TextInput placeholder="Password"
+          placeholderTextColor={'gray'}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry />
+      </Animated.View>
+
+
+      <Animated.View entering={FadeInUp.delay(400).duration(2000).springify()} className="bg-black/5 p-4 rounded-2xl w-full">
+        <TextInput placeholder="Confirm Password"
+          placeholderTextColor={'gray'}
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry />
+      </Animated.View>
+
+      <Animated.View entering={FadeInUp.delay(600).duration(2000).springify()} className="w-full">
         <TouchableOpacity
-          title="Submit"
-          onPressIn={handlePressIn}
-          onPressOut={handlePressOut}
+          className="w-full bg-sky-400 p-3 rounded-2xl mb-2"
+          title = "Submit"
           onPress={handleSignup}
-          style={[styles.buttonContainer, { backgroundColor: buttonColor }]}
         >
-          <Text style={styles.button}>
-            Submit
+          <Text className="text-xl font-bold text-white text-center">
+            Signup
           </Text>
         </TouchableOpacity>
+      </Animated.View>
 
-      </View>
-    </LinearGradient>
-  </KeyboardAvoidingView>
+      <Animated.View entering={FadeInUp.delay(800).duration(2000).springify()} className="flex-row justify-centre">
+        <Text>
+          Already have an account?
+        </Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text className="text-sky-600">
+            Login
+          </Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </View>
+{/* </KeyboardAvoidingView> */}
+  </View>
+</View>
+// </KeyboardAvoidingView>
   );
 };
 
